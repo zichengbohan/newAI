915290750169d64e7ffd94fbdc7b9441d9e1c2d7
xbmld@126.com <github >
2023-09-09 21:54:45 +0800

    add AVSpeechSynthesizer

--- AISleepStory/ContentView.swift
+++ AISleepStory/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    let synthesizer = AVSpeechSynthesizer() // 创建语音合成器
+    @State private var textToSpeak: String = "Hello, this is a text-to-speech example."
+    init() {
+        AVSpeechSynthesisVoice.speechVoices() // <--  fetch voice dependencies
+      }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Text-to-Speech").font(.title)
+            TextEditor(text: $textToSpeak)
+                .padding()
+                .border(Color.gray, width: 1)
+            Button {
+                speakText()
+                
+            } label: {
+                Text("Speak")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.blue)
+                    .cornerRadius(12)
+            }
         }
         .padding()
+    }
+    
+    func speakText() {
+        let speechUtterance = AVSpeechUtterance(string: textToSpeak)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        self.synthesizer.speak(speechUtterance)
+        
     }
 }
 
