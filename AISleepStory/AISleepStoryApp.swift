60223b37dbe6cfcfe3947278c332bc09b67f2338
xbmld@126.com <github >
2023-09-09 17:37:21 +0800

    Initial Commit

---
+++ AISleepStory/AISleepStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISleepStoryApp.swift
+//  AISleepStory
+//
+//  Created by xbm on 2023/9/9.
+//
+
+import SwiftUI
+
+@main
+struct AISleepStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
